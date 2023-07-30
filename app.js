cae6fb891b38255c1f13b3e9312a6ed5900ea20b
Rishab <rishab1319@gmail.com>
2023-07-30 00:49:03 -0400

    Copied data from old rep

--- app.js
+++ app.js
@@ -1,37 +1,42 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const PostRoutes = require('./routes/PostRoutes');
-const UserRoutes = require('./routes/UserRoutes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const auth = require("./routes/auth");
+const user = require("./routes/user");
+const post = require("./routes/post");
+const errorHandler = require("./middlewares/errorHandler");
+const rateLimit = require("express-rate-limit");
+
 const app = express();
 
-
-/*mongoose.connect('mongodb+srv://admin:testing123@cluster0.xqgocdg.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.connect('mongodb+srv://admin:testing123@cluster0.xqgocdg.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
     .then(() => {
         console.log("Connection established successfully");
     })
     .catch(() => {
         console.log("Error while establishing connection to mongodb");
     })
-*/
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static("images"));
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    next();
+
+const apiLimiter = new rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minute
+  max: 100, // limit each IP to 100 requests per windowMs
 });
+app.use("/api/", apiLimiter);
 
-module.exports = app.post("/saveUser",  routes.saveUser);
-module.exports = app.post("/login", userController.login);
-module.exports = app.get("/users", userController.getAllUsers);
-module.exports = app.delete('/users/:id', userController.deleteUser);
-module.exports = app.put('/users/:id', userController.updateUser);
+var corsOptions = {
+  origin: process.env.URI || "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
 
-app.use("/api/posts",PostRoutes);
-app.use("/api/users",UserRoutes);
+app.use("/api/v1/auth", cors(corsOptions), auth);
+app.use("/api/v1/users", cors(corsOptions), user);
+app.use("/api/v1/posts", cors(corsOptions), post);
 
-module.exports = app;
+app.use(errorHandler);
+
+module.exports = { app };
+
+
+
+
---
+++ controllers/auth.js
@@ -0,0 +1,53 @@
+const User = require("../models/User");
+const asyncHandler = require("../middlewares/asyncHandler");
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return next({
+      message: "Please provide email and password",
+      statusCode: 400,
+    });
+  }
+
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return next({
+      message: "The email is not yet registered to an accout",
+      statusCode: 400,
+    });
+  }
+
+  const match = await user.checkPassword(password);
+
+  if (!match) {
+    return next({ message: "The password does not match", statusCode: 400 });
+  }
+  const token = user.getJwtToken();
+  res.status(200).json({ success: true, token });
+});
+
+exports.signup = asyncHandler(async (req, res, next) => {
+  const { username, password, email } = req.body;
+  //console.log( {username, password});
+  console.log(req.body);
+
+  const user = await User.create({ username, password, email });
+  const token = user.getJwtToken();
+  //console.log(token);
+
+  res.status(201).json({ success: true, token });
+});
+
+exports.me = asyncHandler(async (req, res, next) => {
+  const { avatar, username, fullname, email, _id, website, bio } = req.user;
+
+  res
+    .status(200)
+    .json({
+      success: true,
+      data: { avatar, username, fullname, email, _id, website, bio },
+    });
+});
